ceb58f602a9d3c628a2e3411739cc01391f6e4a5
Max Cong <max.cong@nokia-sbell.com>
2021-03-18 08:25:38 +0800

    Revert "Update test_tree.cpp"
    This reverts commit 09aa1867b91af912d8d114afe11b6b7e74d61c21.

--- tst/test_tree.cpp
+++ tst/test_tree.cpp
@@ -10,7 +10,6 @@ using std::tie;
 using radixtree::HandleFunc;
 using radixtree::RadixTree;
 using radixtree::Request;
-using namespace radixtree;
 
 HandleFunc handle = [](Request *req) {
     cout << "Handled! The params are:" << endl;
@@ -20,20 +19,20 @@ HandleFunc handle = [](Request *req) {
 };
 
 RadixTree tree;
-auto req = new Request(std::string("/user/Lime/male/25/tests/"), httpMethod::GET);
+auto req = new Request("/user/Lime/male/25", "GET");
 
 void testWork()
 {
     bool pathExisted;
     HandleFunc handler;
 
-    cout << "1"<<tree.insert("/user", handle, {httpMethod::GET}) << endl;
-    cout << "2"<<tree.insert("/user/:name", handle, {httpMethod::GET}) << endl;
-    cout << "3"<<tree.insert("/user/:name", handle, {httpMethod::GET}) << endl;
-    cout << "4"<<tree.insert("/user/:name/:sex/:age/tests", handle, {httpMethod::GET}) << endl;
-    cout << "5"<<tree.insert("/user/lime", handle, {httpMethod::GET}) << endl;
-    cout << "6"<<tree.insert("/src/*filename", handle, {httpMethod::GET}) << endl;
-    cout << "7"<<tree.insert("/src/image.png", handle, {httpMethod::GET}) << endl;
+    cout << tree.insert("/user", handle, {"GET"}) << endl;
+    cout << tree.insert("/user/:name", handle, {"GET"}) << endl;
+    cout << tree.insert("/user/:name", handle, {"GET"}) << endl;
+    cout << tree.insert("/user/:name/:sex/:age", handle, {"GET"}) << endl;
+    cout << tree.insert("/user/lime", handle, {"GET"}) << endl;
+    cout << tree.insert("/src/*filename", handle, {"GET"}) << endl;
+    cout << tree.insert("/src/image.png", handle, {"GET"}) << endl;
 
     tie(pathExisted, handler, req->params) = tree.get(req->path, req->method);
 
@@ -45,8 +44,8 @@ void testWork()
 void testPerformance()
 {
     auto start = clock();
-    for (auto i = 0; i < 1000000; i++)
-        tree.get("/user/Lime/male/25", httpMethod::GET);
+    for (auto i = 0; i < 10000000; i++)
+        tree.get("/user/Lime/male/25", "GET");
     auto end = clock();
 
     cout << "Total cost " << (end - start) * 1.0 / CLOCKS_PER_SEC
